1d36995e5f3cc440c164a8af7f803ae6d0ce30f3
MuhammadZain-12 <zainshakeel65@gmail.com>
2023-10-03 20:06:03 +0500

    update cancelled ride history

--- src/Screens/History/index.js
+++ src/Screens/History/index.js
@@ -58,44 +58,34 @@ function History({ navigation }) {
 
 
 
-    // const getCancelRidesData = () => {
-    //     setLoading(true);
-    //     firestore()
-    //         .collection('RideCancel')
-    //         .get()
-    //         .then(doc => {
+    const getCancelRidesData = () => {
+        setLoading(true);
+        const id = auth().currentUser.uid;
 
-    //             console.log(doc._docs, "doc")
+        firestore().collection('CancelRides').doc(id).get()
+            .then(doc => {
 
-    //             if (doc._docs[0]._exists) {
 
-    //                 const id = auth().currentUser.uid;
-    //                 let data = doc._docs;
 
-    //                 let myNames = [];
-    //                 data = data.forEach((e, i) => {
-    //                     let myData = e._data.cancelledRides;
-    //                     myData &&
-    //                         myData.length > 0 &&
-    //                         myData.map((j, ind) => {
-    //                             if (j.passengerData.id == id) {
-    //                                 myNames.push(j);
-    //                             }
-    //                         });
-    //                 });
-    //                 let sortedBookings = myNames.length > 0 && myNames.sort((a, b) => (a.date).toDate().getTime() - (b.date).toDate().getTime());
-    //                 sortedBookings = sortedBookings.reverse()
-    //                 setCancelledBookingData(sortedBookings.length > 0 ? sortedBookings : []);
-    //                 setLoading(false);
-    //             } else {
-    //                 setLoading(false)
-    //             }
-    //         });
-    // };
+
+                if (doc._exists) {
+                    let data = doc?.data();
+
+                    console.log(data, "dataaa")
+                    let sortedBookings = data?.cancelledBookings.length > 0 && data?.cancelledBookings?.sort((a, b) => (a.cancelledTime).toDate().getTime() - (b.cancelledTime).toDate().getTime());
+                    sortedBookings = sortedBookings.length > 0 && sortedBookings.reverse()
+                    setCancelledBookingData(sortedBookings.length > 0 ? sortedBookings : []);
+                    setLoading(false);
+                } else {
+                    setLoading(false)
+                }
+
+            });
+    };
 
     useEffect(() => {
         getBookingData();
-        // getCancelRidesData();
+        getCancelRidesData();
     }, []);
 
     const activateTab = index => {
@@ -122,26 +112,6 @@ function History({ navigation }) {
         let date = item?.requestDate?.toDate();
         let stringDate = date.toString();
         stringDate = stringDate.slice(0, 15);
-
-        // let fare = null;
-        // if (
-        //     item.driverData &&
-        //     item.driverData.bidFare &&
-        //     item.driverData.bidFare > 0
-        // ) {
-        //     console.log(item.driverData.bidFare, 'bidFare');
-        //     item.passengerData.selectedCar[0].carMiles.map((e, i) => {
-        //         if (
-        //             item.passengerData.distance >= e.rangeMin &&
-        //             item.passengerData.distance <= e.rangeMax
-        //         ) {
-        //             let serviceCharges = e.serviceCharge;
-        //             let creditCardFee = (Number(item.passengerData.fare) * 5) / 100;
-        //             let totalCharges = Number(serviceCharges) + Number(creditCardFee);
-        //             fare = (Number(item.driverData.bidFare) + totalCharges).toFixed(2);
-        //         }
-        //     });
-        // }
 
         return (
 
@@ -219,7 +189,6 @@ function History({ navigation }) {
                     <View style={{ flexDirection: "row", padding: 5, alignItems: "center" }} >
 
                         <Image source={require("../../Images/Location3.png")} />
-
                         <Text style={{ color: "#808080", fontSize: 14, fontFamily: "Poppins-Medium", marginLeft: 10 }} >Pickup Address: {item.pickupAddress}</Text>
 
                     </View>
@@ -261,82 +230,124 @@ function History({ navigation }) {
 
 
 
-    console.log(bookingData, "bookingData")
-
     const renderCancelBookingData = ({ item, index }) => {
-
-        console.log(item, "items")
-
-        let fare = null;
-        let date = item?.date?.toDate();
-        let stringDate = date?.toString().slice(0, 15);
-        if (
-            item.driverData &&
-            item.driverData.bidFare &&
-            item.driverData.bidFare > 0
-        ) {
-            console.log(item.driverData.bidFare, 'bidFare');
-            item.passengerData.selectedCar[0].carMiles.map((e, i) => {
-                if (
-                    item.passengerData.distance >= e.rangeMin &&
-                    item.passengerData.distance <= e.rangeMax
-                ) {
-                    let serviceCharges = e.serviceCharge;
-                    let creditCardFee = (Number(item.passengerData.fare) * 5) / 100;
-                    let totalCharges = Number(serviceCharges) + Number(creditCardFee);
-                    fare = (Number(item.driverData.bidFare) + totalCharges).toFixed(2);
-                }
-            });
-        }
+        let date = item?.requestDate?.toDate();
+        let stringDate = date.toString();
+        stringDate = stringDate.slice(0, 15);
 
         return (
-            <View>
-                <TouchableOpacity
-                    style={{
-                        alignItems: 'flex-start',
-                        width: '100%',
-                        paddingHorizontal: 30,
-                        borderBottomWidth: 1,
-                        borderColor: Colors.primary,
-                    }}
-                    onPress={() =>
-                        navigation.navigate('PassengerHistorySingleData', {
-                            item,
-                            fare: fare,
-                        })
-                    }
-                >
-                    {/* Date is mentioned Here */}
-                    <Text style={[styles.text, { marginTop: 5 }]}>{stringDate}</Text>
-                    <Text style={[styles.text, { marginTop: 5 }]}>{item?.passengerData?.bookingId}</Text>
-                    <Text
-                        style={[styles.text, { paddingTop: 5, fontSize: 14 }]}
-                        numberOfLines={1}
-                    >
-                        {item.passengerData.pickupAddress}
-                    </Text>
-                    <Text
-                        style={[styles.text, { paddingTop: 5, fontSize: 14 }]}
-                        numberOfLines={1}
-                    >
-                        {item.passengerData.dropOffAddress}
-                    </Text>
-                    <Text
-                        style={[
-                            styles.text,
-                            { paddingTop: 5, fontSize: 14, marginBottom: 10 },
-                        ]}
-                    >
-                        Fare:{' '}
-                        &#x20AC;{
-                            item.passengerData.bidFare
-                                ? item.passengerData.bidFare
-                                : item.passengerData.fare}
 
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        );
+            <TouchableOpacity style={{ marginBottom: 20, width: Dimensions.get("window").width - 30, marginTop: 10 ,alignSelf:"center"}} >
+
+                <Text style={{ color: Colors.buttonColor, fontSize: 18, fontFamily: "Poppins-SemiBold" }} >
+                    {stringDate}
+                </Text>
+
+                <View style={{ marginTop: 10, padding: 10, backgroundColor: "#e6e6e6", borderRadius: 8 }} >
+
+                    <TouchableOpacity style={{ padding: 5, flexDirection: "row", justifyContent: "space-between", backgroundColor: "#e6e6e6", borderRadius: 10, alignItems: "center" }}  >
+                        <View style={{ flexDirection: "row", alignItems: "center" }} >
+                            <Image source={{ uri: item?.driverData?.profile }} style={{ width: 60, height: 60, borderRadius: 10 }} />
+
+                            <View style={{ marginLeft: 5, justifyContent: "center" }} >
+                                <View style={{ flexDirection: "row", alignItems: "center" }} >
+                                    <Text style={{ fontFamily: "Poppins-Medium", fontSize: 18, color: Colors.black, height: 20 }} >{item.driverData?.fullName}</Text>
+                                    <Image source={require("../../Images/star.png")} style={{ marginLeft: 5, marginTop: 5 }} />
+                                    <Text style={{ fontFamily: "Poppins-Regular", fontSize: 14, color: Colors.black, height: 20, marginTop: 5, marginLeft: 3 }} >({item?.driverData?.rating})</Text>
+                                </View>
+                                <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16, color: Colors.gray, height: 20 }} >{item.driverData?.VehicleDetails?.vehicleName}</Text>
+                                <Text style={{ fontSize: 12, color: Colors.white, borderRadius: 30, backgroundColor: "#808080", textAlign: "center", marginTop: 5, padding: 0, width: 80, padding: 2 }} >{item.driverData?.VehicleDetails?.vehicleModelNum}</Text>
+
+                            </View>
+
+                        </View>
+
+                        <View style={{ alignItems: "flex-end" }} >
+
+                            <Text style={{ fontFamily: "Poppins-SemiBold", color: Colors.black, fontSize: 22 }} >${item.fare}</Text>
+
+                            <View style={{ flexDirection: "row" }} >
+
+                                <View style={{ width: 40, height: 40, backgroundColor: Colors.buttonColor, justifyContent: "center", alignItems: "center", borderRadius: 100 }} >
+
+
+                                    <TouchableOpacity onPress={() => Linking.openURL(`tel:${item.driverData?.mobileNumber}`)} >
+                                        <Image source={require("../../Images/phone.png")} />
+                                    </TouchableOpacity>
+                                </View>
+
+
+                                <View style={{ width: 40, height: 40, backgroundColor: Colors.gray, justifyContent: "center", alignItems: "center", borderRadius: 100, marginLeft: 5 }} >
+                                    <TouchableOpacity>
+                                        <Image source={require("../../Images/message.png")} />
+                                    </TouchableOpacity>
+                                </View>
+
+                            </View>
+
+
+                        </View>
+
+
+                    </TouchableOpacity>
+
+
+                    <View style={{ flexDirection: "row", padding: 5, alignItems: "center" }} >
+
+                        <Image source={require("../../Images/Location3.png")} />
+
+                        <Text style={{ color: "#808080", fontSize: 14, fontFamily: "Poppins-Medium", marginLeft: 10 }} >Booking Id: {item?.bookingId}</Text>
+
+                    </View>
+
+                    <View style={{ flexDirection: "row", padding: 5, alignItems: "center" }} >
+
+                        <Image source={require("../../Images/Location3.png")} />
+
+                        <Text style={{ color: "#808080", fontSize: 14, fontFamily: "Poppins-Medium", marginLeft: 10 }} >Booking Type: {item?.bookingType}</Text>
+
+                    </View>
+
+                    <View style={{ flexDirection: "row", padding: 5, alignItems: "center" }} >
+
+                        <Image source={require("../../Images/Location3.png")} />
+                        <Text style={{ color: "#808080", fontSize: 14, fontFamily: "Poppins-Medium", marginLeft: 10 }} >Pickup Address: {item.pickupAddress}</Text>
+
+                    </View>
+
+
+                    <View style={{ flexDirection: "row", padding: 5, alignItems: "center" }} >
+
+                        <Image source={require("../../Images/Location3.png")} />
+
+                        <Text style={{ color: "#808080", fontSize: 14, fontFamily: "Poppins-Medium", marginLeft: 10 }} >Dropoff Address: {item.dropoffAddress}</Text>
+
+                    </View>
+
+                    {item?.bookingType == "twoWay" && <View style={{ flexDirection: "row", padding: 5, alignItems: "center" }} >
+
+                        <Image source={require("../../Images/Location3.png")} />
+
+                        <Text style={{ color: "#808080", fontSize: 14, fontFamily: "Poppins-Medium", marginLeft: 10 }} >Pickup Return Address: {item.returnPickupAddress}</Text>
+
+                    </View>}
+
+
+                    {item?.bookingType == "twoWay" && <View style={{ flexDirection: "row", padding: 5, alignItems: "center" }} >
+
+                        <Image source={require("../../Images/Location3.png")} />
+
+                        <Text style={{ color: "#808080", fontSize: 14, fontFamily: "Poppins-Medium", marginLeft: 10 }} >Dropoff Return Address: {item.returnDropoffAddress}</Text>
+
+                    </View>}
+
+                </View>
+
+
+
+            </TouchableOpacity>
+        )
+
     };
 
     const firstRoute = useCallback(() => {
@@ -402,12 +413,12 @@ function History({ navigation }) {
                         </Text>
                     </View>
                 ) : (
-                    ""
-                    // <FlatList
-                    //     data={cancelledBookingData}
-                    //     renderItem={renderCancelBookingData}
-                    //     keyExtractor={(items, index) => index}
-                    // />
+
+                    <FlatList
+                        data={cancelledBookingData}
+                        renderItem={renderCancelBookingData}
+                        keyExtractor={(items, index) => index}
+                    />
                 )}
             </View>
         );
